b01a589dfe3dad15ce54c1acda826e182c91d59b
Viral Amin <vmanamino@gmail.com>
2015-03-30 01:09:35 +0000

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,37 @@
-require 'faker'
-
-# Create Posts
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
+# checks for unique post and creates one if non existent
+if (Post.where(title: 'my unique title', body: 'my unique body').exists?)
+  p = Post.where(title: 'my unique title', body: 'my unique body')
+  puts "my unique post exists"
+  puts p.to_json
+  
+else
+  unique = Post.create!(
+    title: 'my unique title',
+    body: 'my unique body'
     )
-end
-posts = Post.all
-
-#create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-    )
+  puts "unique post was created"
+  puts unique.to_json
+  
 end
 
-puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+# assign unique post to variable, and call pluck method to get the id only
+postUnique = Post.where(title: 'my unique title', body: 'my unique body')
+pId = postUnique.pluck(:id)
+
+#test for unique comment, create one if non existent.
+if (Comment.where(body: 'my unique comment').exists?)
+  c = Comment.where(body: 'my unique comment')
+  puts "my unique comment exists"
+  puts c.to_json
+  
+else
+  unique_comment = Comment.create!(
+    post_id: pId,
+    body: 'my unique comment'
+    )
+  puts "unique comment was created"
+  puts unique_comment.to_json
+  
+end
+
+
