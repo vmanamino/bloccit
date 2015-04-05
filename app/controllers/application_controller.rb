a2c5cba94d069b767be8820f4265855f5beca2b1
Viral Amin <vmanamino@gmail.com>
2015-04-05 05:43:51 +0000

    Complete authentication continued assignment"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
    def configure_permitted_parameters
      devise_parameter_sanitizer.for(:sign_up) << :name
    end
+  
+   before_action :flash_attack
+     
+  def flash_attack
+    flash[:notice] = "flash attack"
+  end
 end
