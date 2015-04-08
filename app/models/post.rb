4f0ed83e62a58aeb1b3d322caafb01932aef3a7f
Viral Amin <vmanamino@gmail.com>
2015-04-08 05:34:59 +0000

    added two scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   
   default_scope {order('created_at DESC')}
+  scope :ordered_by_title, -> {unscope(:order).order('title ASC')}
+  scope :ordered_by_reverse_created_at, -> {unscope(:order).order('created_at ASC')}
 end
