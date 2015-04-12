d408b5a72e9da25e0be233a61a0649a6e25369de
Viral Amin <vmanamino@gmail.com>
2015-04-12 06:47:46 +0000

    Associate and nest topics and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,20 @@ require 'faker'
 end
 users = User.all
 
+# Create topics
+15.times do
+  Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -30,12 +40,34 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.skip_reconfirmation!
-user.update_attributes!(
-  email: 'example@hotmail.com',
-  password: 'PASSWORD'
-  )
+# Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+
+# Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+
+# Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+ member.skip_confirmation!
+ member.save!
 
 puts "Seed finished"
 puts "#{User.count} users created"
