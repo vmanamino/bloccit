98062e641c8ca648231f4d4f069d2cacbdbd016e
Viral Amin <vmanamino@gmail.com>
2015-04-26 02:20:10 +0000

    Uploading Images Checkpoint

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,14 +1,18 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:  'AWS',
-    aws_access_key_id:  ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']  
-    
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],    
     }
+  
   config.fog_directory = ENV['AWS_BUCKET']
+  
   config.fog_public = true
 end
 
 # Ref:
 # https://support.cloud.engineyard.com/entries/20996881-Use-CarrierWave-and-Optionally-Fog-to-Upload-and-Store-Files#update3
 # http://stackoverflow.com/questions/7946819/carrierwave-and-amazon-s3
+#ENV['AWS_ACCESS_KEY_ID']
+#ENV['AWS_BUCKET']
+#ENV['AWS_SECRET_ACCESS_KEY']
