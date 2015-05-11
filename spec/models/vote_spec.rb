3d253b9d58c983e50a1338722c1a95aab3baef1f
Viral Amin <vmanamino@gmail.com>
2015-05-11 05:20:18 +0000

    correct vote rspec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,7 +5,12 @@ describe Vote do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
         # your expectations here
-        expect(@vote.value).to eq(1).or eq(-1)
+        v1 = Vote.new(value: 1)
+        expect(v1.valid?).to eq(true)
+        v2 = Vote.new(value: -1)
+        expect(v1.valid?).to eq(true)
+        v3 = Vote.new(value: 3)
+        expect(v3.valid?).to eq(false)
       end
     end
   end
