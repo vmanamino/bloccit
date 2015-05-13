f82aa7c580b7959a5e59f7c0af7cc5151913bb4e
Viral Amin <vmanamino@gmail.com>
2015-05-13 03:20:24 +0000

    added methods to count votes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,18 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   mount_uploader :image, ImageUploader
   
+  def up_votes
+    votes.where(value: 1).count
+  end
+  
+  def down_votes
+    votes.where(value: -1).count
+  end
+  
+  def points
+    votes.sum(:value)
+  end
+  
   default_scope {order('created_at DESC')}
   
   validates :title, length: {minimum: 5}, presence: true
