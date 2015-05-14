c823ce99730860d394e5c35a61866276b1559c56
Viral Amin <vmanamino@gmail.com>
2015-05-14 05:06:19 +0000

    updated views and partials

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   
   resources :posts, only: [] do
     resources :comments, only: [:new, :create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
   
   get 'about' => 'welcome#about'
