79cd34eb105e6dc2e02dded0e44375a5ee79fd9a
Viral Amin <vmanamino@gmail.com>
2015-05-15 04:20:50 +0000

    checkpoint complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,10 +17,17 @@ class Post < ActiveRecord::Base
     votes.sum(:value)
   end
   
-  default_scope {order('created_at DESC')}
+  default_scope {order('rank DESC')}
   
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
   ##validates :topic, presence: true
   ##validates :user, presence: true
+  
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    new_rank = points + age_in_days
+      
+    update_attribute(:rank, new_rank)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511012944) do
+ActiveRecord::Schema.define(version: 20150515024654) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150511012944) do
     t.integer  "user_id"
     t.integer  "topic_id"
     t.string   "image"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
