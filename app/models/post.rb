7c284dec72e431979041770f1faf88f163930267
Viral Amin <vmanamino@gmail.com>
2015-05-17 02:22:14 +0000

    validation run

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,8 +23,8 @@ class Post < ActiveRecord::Base
   
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  ##validates :topic, presence: true
-  ##validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
   
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
