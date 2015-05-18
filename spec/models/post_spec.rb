311af0f7c7ef6078032cbc748f1e1251b400b3e8
Viral Amin <vmanamino@gmail.com>
2015-05-18 16:31:39 +0000

    checkpoint complete

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,5 @@
  require 'rails_helper'
+ include TestFactories
  
  describe Post do
    describe "vote methods" do
@@ -38,21 +39,4 @@
    end   
  end
 
-def associated_post(options={})
- post_options = {
-   title: 'Post title',
-   body: 'Post bodies must be pretty long.',
-   topic: Topic.create(name: 'Topic name'),
-   user: authenticated_user
-   }.merge(options)
-  Post.create(post_options)
-end
-
-def authenticated_user(options={})
-  user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
-  user = User.new(user_options)
-  user.skip_confirmation!
-  user.save
-  user
-end
 
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include TestFactories
 
 describe Vote do
   describe "validations" do
@@ -14,4 +15,14 @@ describe Vote do
       end
     end
   end
+  
+  describe 'after_save' do
+    it "calls 'Post#update_rank' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end
 end
+
