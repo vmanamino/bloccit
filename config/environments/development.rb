3f6450ad8eaf13a4e026bbee11886cc90b11d300
Viral Amin <vmanamino@gmail.com>
2015-05-22 20:11:17 +0000

    updated sendgrid password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = {host: 'https://lite.nitrous.io/app#/boxes/207940/ide'}
+  config.action_mailer.default_url_options = {host: 'http://philter-207940.use1.nitrousbox.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   
