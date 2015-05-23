3d63cd1219d799a44201f88a7a42387ab03d4f7e
Viral Amin <vmanamino@gmail.com>
2015-05-23 21:11:56 +0000

    assignment complete

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -17,18 +17,18 @@ describe FavoritesController do
       
       post :create, {post_id: @post.id}
       
-      expect( @user.favorites.find_by_post_id(@post.id)).not_to be_nil
+      expect( @user.favorites.find_by(post_id: @post.id)).not_to be_nil
     end
   end
   
   describe '#destroy' do
     it "destroys the favorite for the current user and post" do
       favorite = @user.favorites.where(post: @post).create
-      expect( @user.favorites.find_by_post_id(@post.id)).not_to be_nil
+      expect( @user.favorites.find_by(post_id: @post.id)).not_to be_nil
       
       delete :destroy, {post_id: @post.id, id: favorite.id}
       
-      expect( @user.favorites.find_by_post_id(@post.id)).to be_nil
+      expect( @user.favorites.find_by(post_id: @post.id)).to be_nil
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,19 @@ require 'rails_helper'
    include TestFactories
  
    describe "#favorited(post)" do
-     xit "returns `nil` if the user has not favorited the post" do
+     
+     before do
+       @user = authenticated_user
+       @post = associated_post  
+       
+     end
+     it "returns `nil` if the user has not favorited the post" do
+       expect(@user.favorited(@post)).to be_nil
      end
  
-     xit "returns the appropriate favorite if it exists" do
+     it "returns the appropriate favorite if it exists" do
+       favorite = @user.favorites.where(post: @post).create
+       expect(@user.favorited(@post)).to eq(favorite)
      end
    end
  end
