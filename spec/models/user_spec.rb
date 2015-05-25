b99c64c5ac93efd70e0b65e0df90b249248eb929
Viral Amin <vmanamino@gmail.com>
2015-05-25 17:19:30 +0000

    completed user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ require 'rails_helper'
      before do
        @user = authenticated_user
        @post = associated_post  
+       @post_other = associated_post
        
      end
      it "returns `nil` if the user has not favorited the post" do
@@ -18,6 +19,12 @@ require 'rails_helper'
      it "returns the appropriate favorite if it exists" do
        favorite = @user.favorites.where(post: @post).create
        expect(@user.favorited(@post)).to eq(favorite)
+     end
+     
+     it "returns `nil` if the user has favorited another post" do
+       favorite_other = @user.favorites.where(post: @post_other).create
+       expect(@user.favorited(@post)).to be_nil
+       
      end
    end
  end
