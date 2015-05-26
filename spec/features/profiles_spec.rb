a69237ea21270f4a7575ea08aeb5202f4a70575b
Viral Amin <vmanamino@gmail.com>
2015-05-26 04:01:05 +0000

    assignment complete

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe "Visiting profiles" do
 
   include TestFactories
-
+  
+  include Warden::Test::Helpers
+  Warden.test_mode!
+  Warden.test_reset!
+  
   before do 
     @user = authenticated_user
     @post = associated_post(user: @user)
@@ -22,5 +26,26 @@ describe "Visiting profiles" do
       expect(page).to have_content(@comment.body)
     end
     
+  end
+  
+  describe "signed in to own profile" do
+    
+    before do
+      @user = authenticated_user
+      login_as(@user, :scope => :user) 
+      @post = associated_post(user: @user)
+      @comment = Comment.new(user: @user, post: @post, body: "A Comment")
+      allow(@comment).to receive(:send_favorite_emails)
+      @comment.save!
+    end
+    
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)      
+    end
+    Warden
   end
 end
