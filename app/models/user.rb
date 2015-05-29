9ff9e191a12b2f263b4c6ea0c0bd6c00f2926fb5
Viral Amin <vmanamino@gmail.com>
2015-05-29 02:50:11 +0000

    applied grouping conventions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  
+  mount_uploader :avatar, AvatarUploader
+  
   has_many :posts, :autosave => true, dependent: :destroy
   has_many :comments, :autosave => true, dependent: :destroy
   has_many :votes, dependent: :destroy
-  mount_uploader :avatar, AvatarUploader
+  
+  
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
   def admin?
     role == 'admin'
