6a614f187a5ff8b8f4d2084abc3ec4ebdbc6c0bb
Viral Amin <vmanamino@gmail.com>
2015-06-01 03:20:43 +0000

    checkpoint complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,15 +9,22 @@ class CommentsController < ApplicationController
   
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.build(params.require(:comment).permit(:body))    
+    @comment = current_user.comments.build(comment_params)    
     @comment.post = @post
+    @new_comment = Comment.new
     authorize @comment
     if @comment.save
-      redirect_to [@post.topic, @post], notice: "Comment was saved!"
+      flash[:notice] = "comment was saved"
+      # redirect_to [@post.topic, @post], notice: "Comment was saved!"
     else 
       flash[:error] = "Error creating comment.  Try again"
-      render :new
+      # render :new
     end
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end    
   end
   
   def destroy
@@ -37,6 +44,13 @@ class CommentsController < ApplicationController
       format.js
     end
     
+  end
+  
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
       
   
